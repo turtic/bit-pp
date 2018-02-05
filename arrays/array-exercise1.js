9654aca8d8c3e2058541cf3e2016798b9cd7acaf
BIT Student <classroom@bgit.rs>
2018-02-05 17:59:57 +0100

    arrays

---
+++ arrays/array-exercise1.js
@@ -0,0 +1,26 @@
+// var months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+
+// console.log(months[5],months[9], months[0]);
+
+
+// var days = ["monday", "tuesday", "wednsday", "thursday", "friday", "saturday", "sunday"];
+
+// console.log(days[6]);
+
+
+// var a = [2, -4, 5, -2, -11];
+
+// console.log(a[1], a[3], a[4]);
+
+
+// var a=[5, 15, -5, 20, 12, 18, 72, 14, 9];
+
+// console.log(a[1],a[4], a[5], a[6], a[7]);
+
+
+var a=[[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+
+// console.log(a[0][3]);
+// console.log(a[2]);
+console.log(a[2][1]);
+
---
+++ switch/exercise7.js
@@ -0,0 +1,41 @@
+var day = 2;
+var month = 23;
+var year = 1989;
+
+if (a > 0 && a < 32 && typeof day === "number" && month > 0 && month < 13 && typeof month === "number" && year === "number") {
+
+    if (month === 2 && day < ) { }
+
+    switch (month) {
+
+        case 2:
+
+            if (date === 31 || date === 30 )  { console.log("Date is not valid"); } else if (date===29 && year === "nije prestupna")
+
+            break;
+
+        case 4:
+            if (date === 31) { console.log("Date is not valid"); }
+            break;
+
+        case 6:
+            if (date === 31) { console.log("Date is not valid"); }
+            break;
+
+        case 9:
+            if (date === 31) { console.log("Date is not valid"); }
+            break;
+
+        case 11:
+            if (date === 31) { console.log("Date is not valid"); }
+            break;
+
+
+
+        default:
+            console.log("Input must be a number between 1 and 12")
+    }
+
+
+
+} else { console.log("Date is not valid") }

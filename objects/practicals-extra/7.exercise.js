08ca8bcfdbaf93f5da2705f9df4088edbcd2515c
turtic <mihajlo.turtic@gmail.com>
2018-02-19 22:41:39 +0100

    practical-extra

--- objects/practicals-extra/7.exercise.js
+++ objects/practicals-extra/7.exercise.js
@@ -2,7 +2,8 @@
 // Write a function that checks if a given string contains any digits.
 // Write a function that checks if a given string is a valid hexadecimal color.
 // Write a function that checks if a given number belongs to the interval from 1900 to 2018. 
-// Write a function named validator that returns an object with properties stringValidator, passwordValidator, colorValidator, and yearValidator referencing the functions from a) to d).
+// Write a function named validator that returns an object with properties stringValidator, 
+// passwordValidator, colorValidator, and yearValidator referencing the functions from a) to d).
 
 
 'use strict';
@@ -12,9 +13,13 @@ function isItAllCap(a) {
   var allCap = a.toUpperCase();
 
   if (a === allCap) {
+
     return true;
+
   } else {
+
     return false
+
   }
 
 }
@@ -24,7 +29,9 @@ function areThereNum(a) {
   for (var i = 0; i < a.length; i++) {
 
     if (typeof parseInt(a[i]) === 'number' && parseInt(a[i]) + '' != NaN + '') {
+
       return true
+
     }
   }
 
@@ -32,12 +39,68 @@ function areThereNum(a) {
 
 }
 
-// function interval(a) {
+function trueColor(s) {
 
-//   if ()
+  if (s[0] == '#') {
 
-//   return typeof parseInt('L');
+    s = s.substring(1);
 
-// }
+  }
 
-console.log(areThereNum('LS1DA sSD'));
+  var x = parseInt(s, 16);
+
+  if (!isNaN(x) && s.length == 6) {
+
+    return true
+
+  } else {
+
+    return false
+  }
+
+}
+
+function yearInterval(n) {
+
+  if (n > 1900 && n < 2018) {
+
+    return true;
+
+  } else {
+
+    return false;
+
+  }
+
+}
+
+// Write a function named validator that returns an object with properties stringValidator, 
+// passwordValidator, colorValidator, and yearValidator referencing the functions from a) to d).
+
+function validator() {
+
+  this.stringValidator = isItAllCap;
+  this.passwordValidator = areThereNum;
+  this.colorValidator = trueColor;
+  this.yearValidator = yearInterval;
+
+}
+
+var checker = new validator();
+
+console.log(checker);
+
+console.log(checker.stringValidator('AAAA'));
+console.log(checker.stringValidator('AaAA'));
+
+console.log(checker.passwordValidator('AAAA'));
+console.log(checker.passwordValidator('AAAA1'));
+
+console.log(checker.colorValidator('#ffffff'));
+console.log(checker.colorValidator('#000000'));
+console.log(checker.colorValidator('ffafffasda'));
+
+console.log(checker.yearValidator('1234'));
+console.log(checker.yearValidator('134342'));
+console.log(checker.yearValidator('2000'));
+
